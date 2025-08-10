b3b21a074b32322893298c379c48a70727d149bc
rawandiaaahmed <rdiaa4913@gmail.com>
2025-08-10 17:44:08 +0300

    edit auth ui

--- lib/feature/auth/view/screens/create_new_password.dart
+++ lib/feature/auth/view/screens/create_new_password.dart
@@ -4,6 +4,7 @@ import 'package:flutter_application_1/core/constants/validator.dart';
 import 'package:flutter_application_1/core/theme/app_colors.dart';
 import 'package:flutter_application_1/core/theme/app_style.dart';
 import 'package:flutter_application_1/feature/auth/view/screens/congrate_screen.dart';
+import 'package:flutter_application_1/feature/auth/view/widgets/custom_buttom_forget.dart';
 
 import 'package:flutter_application_1/feature/auth/view/widgets/custom_text_field.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -21,6 +22,16 @@ class _CreateNewPasswordState extends State<CreateNewPassword> {
   final TextEditingController confirmPassword = TextEditingController();
 
   bool obscurePassword = true;
+  @override
+  void dispose() {
+  
+    password.dispose();
+   
+    confirmPassword.dispose();
+   
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -108,22 +119,16 @@ SizedBox(height: 20.h),
                 SizedBox(height: 5.h),
 
     
-                 ElevatedButton(
-                    onPressed: () { Navigator.push(
+                CustomButtomForget(text: 'Safe Password',formKey: formKey,onValid: () {
+                   Navigator.push(
                           context,
                           MaterialPageRoute(
                             builder: (context) => const CongrateScreen(),
                           ),
-                        );},
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(double.infinity, 50.h),
-                      backgroundColor: AppColors.darkblue100,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.r),
-                      ),
-                    ),
-                    child: Text("Save Password", style: AppStyles.primaryHeadLinesStyle,),
-                  ),
+                        );
+                  
+                },
+                )
               ],
             ),
           ),
